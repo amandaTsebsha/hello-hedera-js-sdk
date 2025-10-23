f640cf9ab1e5128291464732f5dcb309f786ec13
Amanda Tsebsha <amynollz153@gmail.com>
2025-10-23 12:27:46 +0200

    Created NFT and Mint

--- create-nft.js
+++ create-nft.js
@@ -8,7 +8,8 @@ const { Client,
     TokenCreateTransaction,
     TokenType,
     TokenSupplyType,
-    TokenAssociateTransaction
+    TokenAssociateTransaction,
+    TokenMintTransaction
 } = require("@hashgraph/sdk");
 require('dotenv').config();
 
@@ -27,9 +28,6 @@ async function environmentSetUp() {
     //Create Hedera Testnet client
     const client = Client.forTestnet();
 
-    // const node = {"127.0.0.1:50211": new AccountId(3)};
-    // const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
-  //Set my account as client operator:
     client.setOperator(myAccountId, myPrivateKey);
 
     //Set default max transaction fee(in Hbar)
@@ -49,5 +47,102 @@ async function environmentSetUp() {
     const receipt = await newAccount.getReceipt(client);
     const newAccountId = receipt.accountId;
     console.log("The new account ID is: " +newAccountId);
+
+
+    // CREATE NFT
+    const supplyKey = PrivateKey.generate();
+
+    const nftCreate = await new TokenCreateTransaction()
+        .setTokenName("Diploma")
+        .setTokenSymbol("GRAD")
+        .setTokenType(TokenType.NonFungibleUnique)
+        .setDecimals(0)
+        .setInitialSupply(0)
+        .setTreasuryAccountId(myAccountId)
+        .setSupplyType(TokenSupplyType.Finite)
+        .setMaxSupply(250)
+        .setSupplyKey(supplyKey)
+        .freezeWith(client);
+
+    console.log(`- Supply Key: ${supplyKey} \n`);
+
+
+    const nftCreateTxSign = await nftCreate.sign(PrivateKey.fromString(myPrivateKey));
+
+    const nftCreateSubmit = await nftCreateTxSign.execute(client);
+
+    const nftCreateRx = await nftCreateSubmit.getReceipt(client);
+
+    const tokenId = nftCreateRx.tokenId;
+
+    console.log(`- Create NFT with Token ID: ${tokenId} \n`);
+
+    //IPFS content identifiers for which we will create an NFT
+    const CID = [
+            Buffer.from(
+                "ipfs://bafyreiao6ajgsfji6qsgbqwdtjdu5gmul7tv2v3pd6kjgcw5o65b2ogst4/metadata.json"
+            ),
+            Buffer.from(
+                "ipfs://bafyreic463uarchq4mlufp7pvfkfut7zeqsqmn3b2x3jjxwcjqx6b5pk7q/metadata.json"
+            ),
+            Buffer.from(
+                "ipfs://bafyreihhja55q6h2rijscl3gra7a3ntiroyglz45z5wlyxdzs6kjh2dinu/metadata.json"
+            ),
+            Buffer.from(
+                "ipfs://bafyreidb23oehkttjbff3gdi4vz7mjijcxjyxadwg32pngod4huozcwphu/metadata.json"
+            ),
+            Buffer.from(
+                "ipfs://bafyreie7ftl6erd5etz5gscfwfiwjmht3b52cevdrf7hjwxx5ddns7zneu/metadata.json"
+            )
+        ];
+        
+    // MINT NEW NATCH OF NFTs
+    const mintTx = await new TokenMintTransaction()
+        .setTokenId(tokenId)
+        .setMetadata(CID)
+        .freezeWith(client);
+    
+
+    // SIGN THE TRANSACTION WITH SUPPLY KEY
+    const mintTxSign = await mintTx.sign(supplyKey);
+
+    const mintTxSubmit = await mintTxSign.execute(client);
+
+    const mintRx = await mintTxSubmit.getReceipt(client);
+
+    console.log(`Create NFT ${tokenId} with serial number: ${mintRx.serials.toString()} \n`);
+
+    // Create the associate transaction
+    const associateAccountTx = await new TokenAssociateTransaction()
+        .setAccountId(newAccountId)
+        .setTokenIds([tokenId])
+        .freezeWith(client)
+        .sign(newAccountPrivateKey);
+
+    const associateAccountTxSubmit = await associateAccountTx.execute(client);
+
+    const associateAccountRx = await associateAccountTxSubmit.getReceipt(client);
+
+    console.log(`- NFT association with Private Account: ${associateAccountRx.status} \n`);
+
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(myAccountId).execute(client);
+    console.log(`- Treasury balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} units of token ID ${tokenId}`);
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+    console.log(`- New balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} units of token ID ${tokenId}`);
+
+    const tokenTransferTx = await new TransferTransaction()
+        .addNftTransfer(tokenId, 1, myAccountId, newAccountId)
+        .freezeWith(client)
+        .sign(PrivateKey.fromString(myPrivateKey));
+
+    const tokenTransferSubmit = await tokenTransferTx.execute(client);
+    const tokenTransferRx = await tokenTransferSubmit.getReceipt(client);
+
+    console.log(`\n- NFT transfer from Treasury to New Account: ${tokenTransferRx.status} \n`);
+
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(myAccountId).execute(client);
+    console.log(`- Treasury balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} units of token ID ${tokenId}`);
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+    console.log(`- New balance: ${balanceCheckTx.tokens.__map.get(tokenId.toString())} units of token ID ${tokenId}`);
 }
 environmentSetUp();
