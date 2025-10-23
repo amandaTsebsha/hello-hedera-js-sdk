62b7bbc5dd5d9d7780107a206eb42f56968f6cfc
Amanda Tsebsha <amynollz153@gmail.com>
2025-10-23 10:07:56 +0200

    Added Token, Status

--- index1.js
+++ index1.js
@@ -4,7 +4,11 @@ const { Client,
     AccountBalanceQuery, 
     Hbar, 
     TransferTransaction, 
-    AccountId
+    AccountId,
+    TokenCreateTransaction,
+    TokenType,
+    TokenSupplyType,
+    TokenAssociateTransaction
 } = require("@hashgraph/sdk");
 require('dotenv').config();
 
@@ -46,19 +50,77 @@ async function environmentSetUp() {
     const newAccountId = receipt.accountId;
     console.log("The new account ID is: " +newAccountId);
 
-    const accountBalance = await new AccountBalanceQuery()
+    // CREATE FUNGIBLE TOKEN (STABLECOIN)
+    let tokenCreateTx = await new TokenCreateTransaction()
+        .setTokenName("USD Bar")
+        .setTokenSymbol("USDB")
+        .setTokenType(TokenType.FungibleCommon)
+        .setDecimals(2)
+        .setInitialSupply(10000)
+        .setTreasuryAccountId(myAccountId)
+        .setSupplyType(TokenSupplyType.Infinite)
+        .setSupplyKey(supplyKey)
+        .freezeWith(client);
+
+    // SIGN WITH TREASURY KEY
+    let tokenCreateSign = await tokenCreateTx.sign(PrivateKey.fromString(myPrivateKey));
+
+    // SUBMIT TRANSACTION
+    let tokenCreateSubmit = await tokenCreateSign.execute(client);
+
+    // GET THE RECEIPT
+    let tokenCreateRx = await tokenCreateSubmit.getReceipt(client);
+
+    // GET TOKEN ID
+    let tokenId = tokenCreateRx.tokenId;
+
+    // LOG THE ID TO CONSOLE
+    console.log(`- Create token with ID: ${tokenId} \n`);
+
+    const transaction = await new TokenAssociateTransaction()
         .setAccountId(newAccountId)
-        .execute(client);
+        .setTokenIds([tokenId])
+        .freezeWith(client);
 
-        console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
+    
+    const signTx = await transaction.sign(newAccountPrivateKey)
 
-        const sendHbar = await new TransferTransaction()
-            .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
-            .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
-            .execute(client);
+    const txResponse = await signTx.execute(client)
 
-        const transactionReceipt = await sendHbar.getReceipt(client);
-        console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
+    const associationReceipt = await txResponse.getReceipt(client)
+
+    const transactionStatus = associationReceipt.status
+
+    console.log("Transaction of association was: "+transactionStatus)
+
+    const transferTransaction = await new TransferTransaction()
+        .addTokenTransfer(tokenId, myAccountId, -10)
+        .addTokenTransfer(tokenId, newAccountId, 10)
+
+    const signTransferTx = await transferTransaction.sign(PrivateKey.fromString(myPrivateKey))
+
+    const transferTxResponse = await signTransferTx.execute(client)
+
+    const transferReceipt = await transferTxResponse.getReceipt(client)
+
+    const transferStatus = transferReceipt.status
+
+    console.log("the status of the token transfer is: "+transferStatus)
+    
+
+    // const accountBalance = await new AccountBalanceQuery()
+    //     .setAccountId(newAccountId)
+    //     .execute(client);
+
+    //     console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
+
+    //     const sendHbar = await new TransferTransaction()
+    //         .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
+    //         .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
+    //         .execute(client);
+
+    //     const transactionReceipt = await sendHbar.getReceipt(client);
+    //     console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
         
 }
 
